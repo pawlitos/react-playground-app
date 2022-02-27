f4f8e2a74a947ca2d47b298095bdf120a5d9ccb6
Paweł Staś <pawelstas@wp.pl>
2022-02-27 21:47:17 +0100

    Added styled-components

---
+++ src/components/Logo.js
@@ -0,0 +1,20 @@
+import styled, { keyframes } from "styled-components";
+
+const rotate = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+`;
+
+const Logo = styled.img`
+  height: 40vmin;
+  pointer-events: none;
+  @media (prefers-reduced-motion: no-preference) {
+    animation: ${rotate} infinite 20s linear;
+  }
+`;
+
+export default Logo;
