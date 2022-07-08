fcd010e54bbaa8d5fbb655887fa5609c24ffb799
Paweł Staś <pawelstas@wp.pl>
2022-07-08 15:01:45 +0200

    Added prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
-import React from "react";
-import { Header } from "./components/Header";
+import React from 'react';
+import { Header } from './components/Header';
 
 const App = () => {
-  return (
-    <Header />
-  );
+  return <Header />;
 };
 
 export default App;
