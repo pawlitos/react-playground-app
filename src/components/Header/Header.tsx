7244f85835914881094beea21f70894c2d2808ea
Paweł Staś <pawelstas@wp.pl>
2022-08-05 15:19:29 +0200

    Added Container component and adjusted styling

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,28 +1,33 @@
 import { Link } from 'react-router-dom';
 import logo from '../../logo.png';
+import { Container } from '../Container';
 import styles from './Header.module.css';
 
 const Header = () => {
   return (
     <header className={styles.header}>
-      <div className={styles.titleContainer}>
-        <img className={styles.logo} src={logo} alt="logo" />
-        <span className={styles.title}>React Playground</span>
-      </div>
-      <ul className={styles.menuList}>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/tab1">Tab1</Link>
-        </li>
-        <li>
-          <Link to="/tab2">Tab2</Link>
-        </li>
-        <li>
-          <Link to="/tab3">Tab3</Link>
-        </li>
-      </ul>
+      <Container>
+        <div className={styles.headerContent}>
+          <div className={styles.titleContainer}>
+            <img className={styles.logo} src={logo} alt="logo" />
+            <span className={styles.title}>React Playground</span>
+          </div>
+          <ul className={styles.menuList}>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/tab1">Tab1</Link>
+            </li>
+            <li>
+              <Link to="/tab2">Tab2</Link>
+            </li>
+            <li>
+              <Link to="/tab3">Tab3</Link>
+            </li>
+          </ul>
+        </div>
+      </Container>
     </header>
   );
 };
--- src/pages/Layout/Layout.tsx
+++ src/pages/Layout/Layout.tsx
@@ -1,11 +1,14 @@
 import { Outlet } from 'react-router-dom';
+import { Container } from '../../components/Container';
 import { Header } from '../../components/Header';
 
 const Layout = () => {
   return (
     <div>
       <Header />
-      <Outlet />
+      <Container>
+        <Outlet />
+      </Container>
     </div>
   );
 };
