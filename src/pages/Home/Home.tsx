9518f75c18a4145c81c0c261b2156437c126eaba
Paweł Staś <pawelstas@wp.pl>
2022-11-04 14:12:20 +0100

    Testing buttons

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,12 @@
+import { Button } from '../../components/Button';
+
 const Home = () => {
   return (
     <div>
       <h1>Home</h1>
+      <Button>Full</Button>{' '}
+      <Button variant="outlined">Outlined</Button>{' '}
+      <Button variant="link">Link</Button>
     </div>
   );
 };
