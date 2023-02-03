78d797197828c4a5bf09b6fe37682a5b56c552fc
Paweł Staś <pawelstas@wp.pl>
2023-02-03 14:16:04 +0100

    Added user welcome message

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,11 +1,13 @@
 import { Button } from '../../components/Button';
+import { useAuth } from '../../modules/auth/hooks';
 
 const Home = () => {
+  const { user } = useAuth();
+
   return (
     <div>
-      <h1>Home</h1>
-      <Button>Full</Button>{' '}
-      <Button variant="outlined">Outlined</Button>{' '}
+      <h1>Hello {user}</h1>
+      <Button>Full</Button> <Button variant="outlined">Outlined</Button>{' '}
       <Button variant="link">Link</Button>
     </div>
   );
