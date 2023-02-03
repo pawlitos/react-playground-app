fa05219fbbc1824b291cc576196d1d58e345b2c3
Paweł Staś <pawelstas@wp.pl>
2023-02-03 14:30:36 +0100

    Added input type button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,11 +2,13 @@ import styles from './Button.module.css';
 
 type Props = {
   children: string;
+  as?: 'button' | 'input';
+  type?: 'button' | 'submit';
   variant?: 'full' | 'outlined' | 'link';
   onClick?: () => void;
 };
 
-const Button = ({ children, variant = 'full', onClick }: Props) => {
+const Button = ({ children, as = 'button', type = 'button', variant = 'full', onClick }: Props) => {
   let className: string = styles.button;
 
   if (variant === 'full') {
@@ -19,6 +21,10 @@ const Button = ({ children, variant = 'full', onClick }: Props) => {
 
   if (variant === 'link') {
     className += ` ${styles.link}`;
+  }
+
+  if (as === 'input') {
+    return <input value={children} type={type} className={className} onClick={onClick} />;
   }
 
   return (
