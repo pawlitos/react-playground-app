3e2ba57e7e7c12fcb3cef4fd3fb24ea2dba4af4b
Paweł Staś <pawelstas@wp.pl>
2023-02-08 16:02:27 +0100

    Added getAlbumPhotos

--- src/services/api/albums.ts
+++ src/services/api/albums.ts
@@ -3,3 +3,7 @@ import httpClient from './httpClient';
 export const getAlbums = () => {
   return httpClient.get('albums');
 };
+
+export const getAlbumPhotos = (albumId: number) => {
+  return httpClient.get(`albums/${albumId}/photos`);
+};
