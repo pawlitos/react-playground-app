8bf71d521f264e4a8b382477de067c0129665678
Paweł Staś <pawelstas@wp.pl>
2023-03-10 14:01:29 +0100

    Added hamburger button

--- src/common/components/Header/Header.tsx
+++ src/common/components/Header/Header.tsx
@@ -16,18 +16,29 @@ const Header = () => {
             <span className={styles.title}>React Playground</span>
           </div>
           <ul className={styles.menuList}>
-            <li>
+            <li className={styles.linkContainer}>
               <Link className={styles.link} to="/">
                 Home
               </Link>
             </li>
-            <li>
+            <li className={styles.linkContainer}>
               <Link className={styles.link} to="/albums">
                 Albums
               </Link>
             </li>
+            <li className={styles.hamburgerContainer}>
+              <button className={styles.linkButtonHamburger}>
+                <svg viewBox="0 0 100 78" width="22" height="22" className={styles.hamburger}>
+                  <g color="var(--default-font)">
+                    <rect width="100" height="18" rx="8" fill="currentcolor"></rect>
+                    <rect y="30" width="100" height="18" rx="8" fill="currentcolor"></rect>
+                    <rect y="60" width="100" height="18" rx="8" fill="currentcolor"></rect>
+                  </g>
+                </svg>
+              </button>
+            </li>
             {user && (
-              <li>
+              <li className={styles.linkContainer}>
                 <button className={styles.linkButton} onClick={logout}>
                   Logout
                 </button>
