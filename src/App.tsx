58ee768b50307ed3ebf8bfe9d07eda8a1912650c
Paweł Staś <pawelstas@wp.pl>
2023-03-17 14:07:00 +0100

    Changed DrawerProvider location

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { ProtectedRoute } from 'common/components';
-import { DrawerProvider } from 'common/components/Drawer';
 import { Layout } from 'common/layouts';
 import { AlbumPage, AlbumsPage } from 'modules/albums';
 import { AuthProvider, Login } from 'modules/auth';
@@ -10,45 +9,43 @@ const App = () => {
   return (
     <BrowserRouter>
       <AuthProvider>
-        <DrawerProvider>
-          <Routes>
-            <Route path="login" element={<Login />} />
-            <Route path="/" element={<Layout />}>
-              <Route
-                index
-                element={
-                  <ProtectedRoute>
-                    <Home />
-                  </ProtectedRoute>
-                }
-              />
-              <Route
-                path="albums"
-                element={
-                  <ProtectedRoute>
-                    <AlbumsPage />
-                  </ProtectedRoute>
-                }
-              />
-              <Route
-                path="albums/:albumId"
-                element={
-                  <ProtectedRoute>
-                    <AlbumPage />
-                  </ProtectedRoute>
-                }
-              />
-            </Route>
+        <Routes>
+          <Route path="login" element={<Login />} />
+          <Route path="/" element={<Layout />}>
             <Route
-              path="*"
+              index
               element={
-                <main style={{ padding: '1rem' }}>
-                  <p>There's nothing here!</p>
-                </main>
+                <ProtectedRoute>
+                  <Home />
+                </ProtectedRoute>
               }
             />
-          </Routes>
-        </DrawerProvider>
+            <Route
+              path="albums"
+              element={
+                <ProtectedRoute>
+                  <AlbumsPage />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="albums/:albumId"
+              element={
+                <ProtectedRoute>
+                  <AlbumPage />
+                </ProtectedRoute>
+              }
+            />
+          </Route>
+          <Route
+            path="*"
+            element={
+              <main style={{ padding: '1rem' }}>
+                <p>There's nothing here!</p>
+              </main>
+            }
+          />
+        </Routes>
       </AuthProvider>
     </BrowserRouter>
   );
--- src/common/layouts/Layout/Layout.tsx
+++ src/common/layouts/Layout/Layout.tsx
@@ -1,15 +1,16 @@
 import { Outlet } from 'react-router-dom';
 import { Container, Drawer, Header } from 'common/components';
+import { DrawerProvider } from 'common/components/Drawer';
 
 const Layout = () => {
   return (
-    <>
+    <DrawerProvider>
       <Drawer />
       <Header />
       <Container>
         <Outlet />
       </Container>
-    </>
+    </DrawerProvider>
   );
 };
 
